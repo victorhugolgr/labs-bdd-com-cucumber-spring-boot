f5122ad875630dbb6125bdfd7dbdbd1bd23a3fee
Victor Hugo <victorhugolgr@gmail.com>
2020-08-25 19:21:32 -0300

    Laboratorio de bdd com cucumber - Spring boot

---
+++ src/test/java/br/com/alura/leilao/acceptance/steps/LeilaoSteps.java
@@ -0,0 +1,50 @@
+package br.com.alura.leilao.acceptance.steps;
+
+import org.junit.Assert;
+
+import br.com.alura.leilao.e2e.pages.Browser;
+import br.com.alura.leilao.e2e.pages.LeiloesPage;
+import br.com.alura.leilao.e2e.pages.LoginPage;
+import br.com.alura.leilao.e2e.pages.NovoLeilaoPage;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class LeilaoSteps {
+
+	private LoginPage loginPage;
+	private LeiloesPage leiloesPage;
+	private NovoLeilaoPage novaLeilaoPage;
+	private Browser browser;
+
+	@Dado("um usuario logado")
+	public void um_usuario_logado() {
+		browser = new Browser();
+		browser.seed();
+		loginPage = browser.getLoginPage();
+		leiloesPage = loginPage.realizaLoginComoFulano();
+	}
+
+	@Quando("acessa a pagina de novo leilao")
+	public void acessa_a_pagina_de_novo_leilao() {
+		novaLeilaoPage = this.leiloesPage.visitaPaginaParaCriarUmNovoLeilao();
+	}
+
+	@Quando("prenche o formulario com dados validos")
+	public void prenche_o_formulario_com_dados_validos() {
+		this.leiloesPage = novaLeilaoPage.preencheForm("PC novo", "15000", "01/11/2020");
+		
+	}
+
+	@Entao("volta para a pagina de leiloes")
+	public void volta_para_a_pagina_de_leiloes() {
+		Assert.assertTrue(this.leiloesPage.estaNaPaginaDeLeiloes());
+	}
+
+	@Entao("o novo leilao aparece na tabela")
+	public void o_novo_leilao_aparece_na_tabela() {
+		Assert.assertTrue(this.leiloesPage.existe("PC novo", "1500", "01/11/2020", "fulano"));
+		this.browser.clean();
+	}
+
+}
